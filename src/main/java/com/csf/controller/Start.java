cde0afa29e1039da14291e55fee723d0332e1619
MaxR00t <15692039518@163.com>
2022-11-13 11:27:13 +0800

    common-lib project init 02

---
+++ src/main/java/com/csf/controller/Start.java
@@ -0,0 +1,9 @@
+package com.csf.controller;
+
+public class Start {
+    public static void main(String[] args) {
+        String name = "max";
+        int year = 28;
+        System.out.println("name: " + name + ", year: " + year);
+    }
+}
